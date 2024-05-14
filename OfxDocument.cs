c400c2a6c7f89c1d129e4dbf03d5ce8fdc1cbd89
Tais Zimerer <taiszimerer@gmail.com>
2024-05-14 16:01:00 -0300

    Add folders

---
+++ OfxDocument.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Cofactory.OFX
+{
+    public class OfxDocument
+    {
+        public string Headers { get; private set; }
+        public string BeforeBody { get; private set; }
+        public Transaction[] Transactions { get; private set; }
+
+        public OfxDocument(string ofxContent)
+        {
+            ParseContent(ofxContent);
+        }
+
+        private void ParseContent(string content)
+        {
+            int sgmlStart = content.IndexOf("<OFX>");
+
+            if (sgmlStart < 0)
+            {
+                throw new Exception("Arquivo OFX inválido. Verifique se o mesmo esta sendo enviado no formato correto.");
+            }
+
+            Headers = content.Substring(0, sgmlStart);
+            content = content.Substring(sgmlStart);
+
+            int startBody = content.IndexOf("<STMTTRN>");
+            string body = content.Substring(startBody);
+            BeforeBody = content.Substring(0, startBody);
+
+            string[] transactions = Regex.Split(body, @"(?=<STMTTRN>)");
+            Transactions = transactions.Select(t => new Transaction(t)).ToArray();
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions = Transactions.Concat(new[] { transaction }).ToArray();
+        }
+        public Transaction[] Result()
+        {
+            return Transactions;
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Cofactory.OFX
+{
+    public class Transaction
+    {
+        public string TransactionType { get; set; }
+        public DateTime PostedDate { get; set; }
+        public decimal TransactionAmount { get; set; }
+        public string FitId { get; set; }
+        public string CheckNumber { get; set; }
+        public string Memo { get; set; }
+        public string RawContent { get; set; }
+
+        public Transaction(string content)
+        {
+            RawContent = content;
+            TransactionType = GetValueFromContent("TRNTYPE");
+            PostedDate = ParsePostedDate(GetValueFromContent("DTPOSTED"));
+            TransactionAmount = ParseTransactionAmount(GetValueFromContent("TRNAMT"));
+            FitId = GetValueFromContent("FITID");
+            CheckNumber = GetValueFromContent("CHECKNUM");
+            Memo = GetValueFromContent("MEMO");
+        }
+        private string GetValueFromContent(string tagName)
+        {
+            var match = Regex.Match(RawContent, $"<{tagName}>(.*?)(<|\n)", RegexOptions.Singleline);
+            if (match.Success)
+            {
+                return match.Groups[1].Value.Trim();
+            }
+            return null;
+        }
+
+        private DateTime ParsePostedDate(string rawDate)
+        {
+            if (rawDate != null && rawDate.Length >= 14)
+            {
+                return DateTime.ParseExact(rawDate.Substring(0, 14), "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                return DateTime.MinValue;
+            }
+        }
+
+        private decimal ParseTransactionAmount(string rawAmount)
+        {
+            if (!string.IsNullOrEmpty(rawAmount))
+            {
+                return decimal.Parse(rawAmount, CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
